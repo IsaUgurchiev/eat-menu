58f8cc79f7d07f93e39f5a5a29c8420f97a265e5
Isa Ugurchiev <isa.ugurchiev@gmail.com>
2018-01-26 10:05:37 +0300

    Добавил в сборщик browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,28 @@ var gulp = require('gulp'),
 	prefix = require('gulp-autoprefixer'),
 	sourcemaps = require('gulp-sourcemaps'),
 	sass = require('gulp-sass'),
-	stylelint = require('gulp-stylelint');
+	stylelint = require('gulp-stylelint'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
+
+var config = {
+	server: {
+		baseDir: "./build"
+	},
+	tunnel: true,
+	host: 'localhost',
+	port: 8080,
+	logPrefix: "Frontend_Devil"
+};
 
 var path = {
 	build: {
-		css: 'build/css/'
+		css: 'build/css/',
+		html: 'build'
 	},
 	src: {
-		style: 'src/scss/**/*.scss'
+		styles: 'src/scss/**/*.scss',
+		html: 'src/index.html'
 	}
 };
 
@@ -22,7 +36,7 @@ var stylelintConfig = {
 
 // SASS Version
 gulp.task('styles', function () {
-	return gulp.src(path.src.style)
+	return gulp.src(path.src.styles)
 		.pipe(stylelint({
 			config: stylelintConfig,
 			syntax: "scss",
@@ -36,12 +50,29 @@ gulp.task('styles', function () {
 		.pipe(concat('main.css'))
 		.pipe(minifyCSS())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.build.css));
+		.pipe(gulp.dest(path.build.css))
+		.pipe(reload({stream: true}));
 });
 
-gulp.task('default', function () {
-	gulp.run('styles');
-	gulp.watch(path.src.style, function () {
-		gulp.run('styles');
-	})
+gulp.task('html', function () {
+	gulp.src(path.src.html)
+		.pipe(gulp.dest(path.build.html))
+		.pipe(reload({stream: true}));
 });
+
+gulp.task('watcher', function () {
+	gulp.watch(path.src.html, ['html']);
+	gulp.watch(path.src.styles, ['styles']);
+});
+
+gulp.task('browserSync', function () {
+	browserSync(config);
+});
+
+gulp.task('build', [
+	'html',
+	'styles'
+]);
+
+gulp.task('default', ['build', 'watcher', 'browserSync']);
+
